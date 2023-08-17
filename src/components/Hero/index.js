6e6986f3a36ab49200bc8bbe1b60fcdd9755581b
Joseph Felker <jayefelker@gmail.com>
2023-08-17 15:28:06 -0500

    refactor navbar

---
+++ src/components/Hero/index.js
@@ -0,0 +1,9 @@
+export default function Hero(){
+
+    return(
+        <div className="relative">
+            <img src="/Sponsor-Images.jpg" className="max-w-full h-auto" alt="Hero"/>
+
+        </div>
+    )
+}
