50ae2ab7ea839221480b6815fb9e882d4f0cb163
Joseph Felker <jayefelker@gmail.com>
2023-08-22 16:45:01 -0500

    create and apply theme

---
+++ src/components/Theme/index.js
@@ -0,0 +1,24 @@
+
+export const themeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#354f52",
+    },
+    secondary: {
+      main: "#52796f",
+    },
+    background: {
+      default: "#fafafa",
+    },
+    error: {
+      main: "#9e2a2b",
+    },
+    warning: {
+      main: "#e09f3e",
+    },
+    info: {
+      main: "#cad2c5",
+    },
+  },
+};
