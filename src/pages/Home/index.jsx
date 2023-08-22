285375ae6ff468ddb1a08f974baee94e4c8df1e5
Joseph Felker <jayefelker@gmail.com>
2023-08-22 15:50:13 -0500

    add mui

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,22 @@
+import { Button } from "@mui/material";
+
+function MyComponent() {
+  return (
+    <Button variant="contained" color="primary">
+      Click me
+    </Button>
+  );
+}
+
+
 export default function Home(){
     return(
         <>
             <div>This is the Home page</div>
+            <MyComponent/>
+            <MyComponent/>
+
+            
         </>
     )
 }
